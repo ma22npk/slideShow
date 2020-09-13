953f6e2c64c0d7ddce45043be9bfc509fc3993fe
Kota Matsumoto <mac@mba.local>
2020-09-14 01:05:01 +0900

    jsでメイン画像を表示

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+{
+const images = [
+'img/1.png',
+'img/2.png',
+'img/3.png',
+'img/4.png',
+'img/5.png',
+'img/6.png',
+'img/7.png'
+];
+let currentIndex = 0;
+
+const mainImages = document.getElementById('main');
+mainImages.src = images[currentIndex];
+
+
+}
