4b183b327c60c853a5cd5ef90b225f6375d2bbe0
Kota Matsumoto <mac@mba.local>
2020-09-14 01:52:38 +0900

    サムネイルをJSで表示

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 'use strict';
 
 {
-const images = [
-'img/1.png',
-'img/2.png',
-'img/3.png',
-'img/4.png',
-'img/5.png',
-'img/6.png',
-'img/7.png'
-];
-let currentIndex = 0;
+  const images = [
+    'img/1.png',
+    'img/2.png',
+    'img/3.png',
+    'img/4.png',
+    'img/5.png',
+    'img/6.png',
+    'img/7.png'
+  ];
+  let currentIndex = 0;
 
-const mainImages = document.getElementById('main');
-mainImages.src = images[currentIndex];
+  const mainImages = document.getElementById('main');
+  mainImages.src = images[currentIndex];
+  images.forEach(image => {
+    const img = document.createElement('img');
+    img.src = image;
 
+    const li = document.createElement('li');
+    li.appendChild(img);
+    document.querySelector('.thumbnails').appendChild(li);
+  });
 
 }
